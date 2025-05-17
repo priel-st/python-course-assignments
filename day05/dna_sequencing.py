f88ee668a4c4fff93e23d32a50ee1e83ebf3e0fa
priel-st <priel.simantov1@gmail.com>
2025-05-17 17:29:22 +0300

    Create dna_sequencing.py

---
+++ day05/dna_sequencing.py
@@ -0,0 +1,13 @@
+seq = input("Please type in a sequence: ")
+seq_2 = ""
+for ch in seq:
+    if ch in ["A", "C", "T", "G"]:
+        seq_2 = seq_2 + ch
+    else:
+        seq_2 = seq_2 + "X"
+seq_3 = seq_2.split("X")
+while "" in seq_3:
+    seq_3.remove("")
+print(seq_3)
+seq_3.sort(key = len, reverse = True)
+print(seq_3)
