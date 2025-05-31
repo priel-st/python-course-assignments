1e271ba06414fe6c3516be1123489b279f8ddffc
priel-st <priel.simantov1@gmail.com>
2025-05-31 16:12:13 +0300

    Update count_digits_in_file.py

--- day06/count_digits_in_file.py
+++ day06/count_digits_in_file.py
@@ -1,18 +1,18 @@
-dict={}
+Dict={}
 with open("numbers.txt", "r") as file:
     for line in file:
         line = line.rstrip ("\n")
         for digit in line:
             if digit==" ":
                 continue
-            elif digit not in dict:
-                dict[digit] = 1
-            elif digit in dict:
-                dict[digit] = dict[digit]+1
+            elif digit not in Dict:
+                Dict[digit] = 1
+            elif digit in Dict:
+                Dict[digit] = Dict[digit]+1
 
 with open('report.txt', 'w') as count:
     for x in "0123456789":
-        if x in dict:
-            count.write("{:<2}{}\n".format(x, dict[x]))
+        if x in Dict:
+            count.write("{:<2}{}\n".format(x, Dict[x]))
         else:
             count.write("{:<2}{}\n".format(x, "0"))
