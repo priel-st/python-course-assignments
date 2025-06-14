b5807a27c4ab7e4735ba6d4e9ffcf04818a0405a
priel-st <priel.simantov1@gmail.com>
2025-06-14 19:24:27 +0300

    Adding documentation

--- day09/dig_count.py
+++ day09/dig_count.py
@@ -1,8 +1,18 @@
 def join_numbers(num_list):
+  """
+  This function joins a list of integers into one string of digits
+  >>>join_numbers([123,455,01])
+  12345501
+  """
   joined = "".join(map(str, num_list))
   return joined
 
 def digit_list(joined):
+  """
+  This function extracts unique digits from a string to an ordered list
+  >>>digit_list(1231230)
+  ['0','1','2','3']
+  """
   L=[]
   for digit in joined:
     if digit not in L:
@@ -11,6 +21,9 @@ def digit_list(joined):
   return L
 
 def count_dig(L, joined):
+  """
+  This function prints the frequency of each digit in the joined string
+  """
   for x in L:
     num = joined.count(x)
     print("{:<2}{}".format(x, num))
